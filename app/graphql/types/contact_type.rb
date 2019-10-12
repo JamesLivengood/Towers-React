17bfdac5b7b6c542f97f5f517837ad9b5d78f732
Nick O'Neill <mr.nick.oneill@gmail.com>
2019-10-12 16:46:11 -0400

    Created the graphql endpoint and allContacts query

---
+++ app/graphql/types/contact_type.rb
@@ -0,0 +1,14 @@
+module Types
+    class ContactType < BaseObject
+        field :id, ID, null: false
+        field :first_name, String, null: false
+        field :last_name, String, null: false
+        field :description_markdown, String, null: true
+        field :primary_email, String, null: true
+        field :secondary_emails, [String], null: true
+        field :phone, String, null: true
+        field :linkedin, String, null: true
+        field :twitter, String, null: true
+        field :instagram, String, null: true
+    end
+end

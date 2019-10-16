ce720141798f186a8a1f87a18632b6e3236531d8
Nick O'Neill <mr.nick.oneill@gmail.com>
2019-10-16 19:19:34 -0400

    Added root path

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/*path' => 'pages#homepage'
-  # root to: 'pages#homepage'
+  root to: 'pages#homepage'
 end

fae4703e304b7747a562535ed273c415eacb11df
Nick O'Neill <mr.nick.oneill@gmail.com>
2019-10-15 21:21:48 -0400

    Reduced homepage footprint

--- client/src/Pages/Homepage/index.tsx
+++ client/src/Pages/Homepage/index.tsx
@@ -1,35 +1,19 @@
 import React, { FC } from 'react';
-
+import { PageWithHeader } from '../../Common/Components/PageWithHeader';
 const Homepage: FC = (props) => {
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-lg-8 col-md-10 col-sm-12">
-                    <nav>
-                        <a href='https://www.nickoneill.com'>nickoneill.com</a>
-                    </nav>
-                    <article className="post">
-                        <header className="post-header">
-                            <h1 className="post-title">The Network</h1>
-                        </header>
-
-                        <div className="post-content">
-                            <p>Welcome!</p>
-                            <p>
-                            This is my personal network of friends and professional collaborators.
-                            </p>
-                            <p>
-                            If you've ended up on this site and aren't already part of the network, <a href='mailto: holler@nickoneill.com'>shoot me an email</a> and introduce yourself!
-                            </p>
-                            <p>
-                            I look forward to connecting!
-                            </p>
-                        </div>
-                    </article>
-                    <p><small>&copy; 2019, <a href='https://www.nickoneill.com'>Nick O'Neill</a></small></p>
-                </div>
-            </div>
-        </div>
+        <PageWithHeader header={"The Network"}>
+            <p>Welcome!</p>
+            <p>
+            This is my personal network of friends and professional collaborators.
+            </p>
+            <p>
+            If you've ended up on this site and aren't already part of the network, <a href='mailto: holler@nickoneill.com'>shoot me an email</a> and introduce yourself!
+            </p>
+            <p>
+            I look forward to connecting!
+            </p>
+        </PageWithHeader>
     );
 };
 

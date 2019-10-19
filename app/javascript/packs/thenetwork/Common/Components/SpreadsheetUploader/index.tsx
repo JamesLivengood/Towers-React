7ae745b8eb4f709649f86605011a680b44794dc7
JamesLivengood <james.livengood@gmail.com>
2019-10-19 14:55:18 -0400

    before deleting all old xlsx stuff

--- app/javascript/packs/thenetwork/Common/Components/SpreadsheetUploader/index.tsx
+++ app/javascript/packs/thenetwork/Common/Components/SpreadsheetUploader/index.tsx
@@ -1,13 +1,11 @@
 import React, { FC } from 'react';
-import XLSX from 'xlsx';
-import excel from 'xlsx';
+import { CSVReader } from 'react-papaparse';
 
 type IProps = {
 };
 
 const SpreadsheetUploader: FC<IProps> = (props) => {
-  const upload = (evt) => {
-    debugger
+  // const upload = (evt) => {
     // var selectedFile = evt.target.files[0];
     // var reader = new FileReader();
     // reader.onload = function (event) {
@@ -29,10 +27,30 @@ const SpreadsheetUploader: FC<IProps> = (props) => {
     // };
 
     // reader.readAsBinaryString(selectedFile);
-  };
+  // };
+
+  const [fileInput, setFileInput] = React.useState(null)
+
+  const handleReadCSV = (data) => {
+    console.log(data);
+  }
+
+  const handleOnError = (err, file, inputElem, reason) => {
+    console.log(err);
+  }
+
+  const handleImportOffer = () => {
+    fileInput.current.click();
+  }
 
   return (
-    <input type="file" id="fileUploader" name="fileUploader" onChange={upload}/>
+    // <input type="file" id="fileUploader" name="fileUploader" onChange={upload}/>
+    <CSVReader
+      onFileLoaded={handleReadCSV}
+      inputRef={fileInput}
+      // style={{ display: 'none' }}
+      onError={handleOnError}
+    />
   )
 }
 

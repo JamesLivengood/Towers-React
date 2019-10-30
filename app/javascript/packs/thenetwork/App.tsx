4444a76aa2d53167ba68ca92b8aa69182168c445
JamesLivengood <james.livengood@gmail.com>
2019-10-29 23:07:38 -0400

    removing apollo

--- app/javascript/packs/thenetwork/App.tsx
+++ app/javascript/packs/thenetwork/App.tsx
@@ -2,20 +2,11 @@ import React from 'react';
 import RoutesContainer from './Routes';
 import './App.css';
 
-import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient from 'apollo-boost';
-
-const client = new ApolloClient({
-  uri: '/graphql',
-});
-
 const App: React.FC = () => {
   return (
-    <ApolloProvider client={client}>
-      <div className="App">
-        <RoutesContainer />
-      </div>
-    </ApolloProvider>
+    <div className="App">
+      <RoutesContainer />
+    </div>
   );
 }
 

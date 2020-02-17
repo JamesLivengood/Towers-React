f73a3db1cd86ffbcf456f6aedaada63cc3a7257a
James Livengood <james.livengood@gmail.com>
2020-02-16 23:45:28 -0500

    Removing possibly vulnerable routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
-  post "/graphql", to: "graphql#execute"
-  get "/map", to: "pages#map"
+  # post "/graphql", to: "graphql#execute"
+
   namespace 'api', defaults: {format: :json} do
-    resources :towers, only: [:create, :index]
-    resources :transmitters, only: [:create, :index]
+    resources :towers, only: [:index]
+    resources :transmitters, only: [:index]
     resources :succesful_downloads, only: [:index]
     resources :failed_downloads, only: [:index]
-    resources :antenna_search_urls, only: [:create]
+    # resources :antenna_search_urls, only: [:create]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

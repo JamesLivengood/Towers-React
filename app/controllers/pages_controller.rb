c48afb3c56e626ec06007d7bc3be5b1bfd351a94
JamesLivengood <james.livengood@gmail.com>
2020-06-11 12:41:29 -0400

    Removing geo data

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,8 +9,8 @@ class PagesController < ActionController::Base
       user_agent: request.user_agent,
       browser_string: browser.to_s,
       ip_address: request.remote_ip,
-      params: params.except(:action, :controller),
-      geo_data: Geocoder.search(request.remote_ip).first.data
+      params: params.except(:action, :controller)
+      # geo_data: Geocoder.search(request.remote_ip).first.data
     )
   end
 end

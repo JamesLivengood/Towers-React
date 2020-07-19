8fb9425f964995c23883621f03fd843fceed9d1c
JamesLivengood <james.livengood@gmail.com>
2020-07-19 19:21:24 -0400

    Config update for site name change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-
-  config.hosts << "towers-n-transmitters.herokuapp.com"
-  config.hosts << "usa-tower-map.herokuapp.com"
+  config.hosts << "my-cell-map.herokuapp.com"
 end
